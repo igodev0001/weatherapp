9aeed9c6a0b09b230ab79b944b11ba1074c85eae
azuredeviil053 <azuredevil053@gmail.com>
2023-09-13 20:09:46 -0300

    fix: stylelint erros

--- src/components/WeatherCard/styles.ts
+++ src/components/WeatherCard/styles.ts
@@ -9,19 +9,22 @@ export const Container = styled.div`
   align-items: center;
   background-color: #d9d9d9;
   border-radius: 0.5rem;
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 10%),
+    0 2px 4px -2px rgb(0 0 0 / 10%);
   display: flex;
   flex-flow: column;
   height: 24rem;
+  justify-content: center;
+  mix-blend-mode: multiply;
   overflow: hidden;
   position: relative;
-  mix-blend-mode: multiply;
-  justify-content: center;
   width: 20rem;
 `;
 
 export const Clouds = styled.img`
   background-size: cover;
-  height: 150%;
+  height: 100%;
   object-fit: cover;
   pointer-events: none;
   position: absolute;
@@ -36,8 +39,8 @@ export const Temperature = styled.h1`
 `;
 
 export const Location = styled.h2`
+  bottom: 4rem;
   font-size: 1.75rem;
   font-weight: 600;
   position: absolute;
-  bottom: 4rem;
 `;
