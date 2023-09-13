b134d98a9e9f9d72a840bddd04781e0165458bce
azuredeviil053 <azuredevil053@gmail.com>
2023-09-13 20:31:47 -0300

    feat & refact: clouds texture and cloud element to WeatherCard component

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import cloudTexture from '@assets/images/cloud_texture.png';
 import clouds from '@assets/images/clouds.png';
 
 import * as S from './styles';
 
 export default function WeatherCard() {
   return (
-    <S.Container>
-      <S.Temperature>60º</S.Temperature>
-      <S.Average>58º / 61º</S.Average>
-      <S.Location>Recife, PE</S.Location>
-
+    <S.Wrapper>
       <S.Clouds src={clouds} />
-    </S.Container>
+
+      <S.Container>
+        <S.Temperature>60º</S.Temperature>
+        <S.Average>58º / 61º</S.Average>
+        <S.Location>Recife, PE</S.Location>
+
+        <S.Texture src={cloudTexture} />
+      </S.Container>
+    </S.Wrapper>
   );
 }
--- src/components/WeatherCard/styles.ts
+++ src/components/WeatherCard/styles.ts
@@ -3,6 +3,16 @@ import styled from 'styled-components';
 export const Average = styled.h3`
   font-size: 1.5rem;
   font-weight: 600;
+  position: absolute;
+  top: 55%;
+`;
+
+export const Clouds = styled.img`
+  height: 4rem;
+  left: -3rem;
+  position: absolute;
+  top: 4rem;
+  z-index: 10;
 `;
 
 export const Container = styled.div`
@@ -18,24 +28,7 @@ export const Container = styled.div`
   justify-content: center;
   mix-blend-mode: multiply;
   overflow: hidden;
-  position: relative;
   width: 20rem;
-`;
-
-export const Clouds = styled.img`
-  background-size: cover;
-  height: 100%;
-  object-fit: cover;
-  pointer-events: none;
-  position: absolute;
-  width: 150%;
-  z-index: -10;
-`;
-
-export const Temperature = styled.h1`
-  font-size: 4rem;
-  font-weight: 600;
-  margin-left: 0.75rem;
 `;
 
 export const Location = styled.h2`
@@ -43,4 +36,25 @@ export const Location = styled.h2`
   font-size: 1.75rem;
   font-weight: 600;
   position: absolute;
+`;
+
+export const Temperature = styled.h1`
+  font-size: 4rem;
+  font-weight: 600;
+  margin-left: 0.75rem;
+  position: absolute;
+  top: 35%;
+`;
+
+export const Texture = styled.img`
+  background-size: cover;
+  height: 100%;
+  object-fit: cover;
+  pointer-events: none;
+  width: 150%;
+  z-index: -10;
+`;
+
+export const Wrapper = styled.div`
+  position: relative;
 `;
