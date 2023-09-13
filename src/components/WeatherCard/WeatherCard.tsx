6089e9c133c159ca17bad5322d6d45afca1431d1
azuredeviil053 <azuredevil053@gmail.com>
2023-09-13 19:54:20 -0300

    feat: WeatherCard skeleton implementation

--- src/components/WeatherCard/WeatherCard.tsx
+++ src/components/WeatherCard/WeatherCard.tsx
@@ -3,5 +3,11 @@ import React from 'react';
 import * as S from './styles';
 
 export default function WeatherCard() {
-  return <S.Container></S.Container>;
+  return (
+    <S.Container>
+      <S.Temperature>60º</S.Temperature>
+      <S.Average>58º / 61º</S.Average>
+      <S.Location>Recife, PE</S.Location>
+    </S.Container>
+  );
 }
--- src/components/WeatherCard/styles.ts
+++ src/components/WeatherCard/styles.ts
@@ -1,8 +1,31 @@
 import styled from 'styled-components';
 
+export const Average = styled.h3`
+  font-size: 1.5rem;
+  font-weight: 600;
+`;
+
 export const Container = styled.div`
+  align-items: center;
   background-color: #d9d9d9;
   border-radius: 0.5rem;
+  display: flex;
+  flex-flow: column;
   height: 24rem;
+  position: relative;
+  justify-content: center;
   width: 20rem;
+`;
+
+export const Temperature = styled.h1`
+  font-size: 4rem;
+  font-weight: 600;
+  margin-left: 0.75rem;
+`;
+
+export const Location = styled.h2`
+  font-size: 1.75rem;
+  font-weight: 600;
+  position: absolute;
+  bottom: 4rem;
 `;
