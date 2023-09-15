120b211ccb7ecb1cb17ccf4b291ccdc7e19a1294
azuredeviil053 <azuredevil053@gmail.com>
2023-09-15 15:51:44 -0300

    feat: typed AxiosResponse with WeatherProps

--- src/api/weatherApi.ts
+++ src/api/weatherApi.ts
@@ -1,13 +1,51 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
-const getWeather = () => {
+type WeatherProps = {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  visibility: number;
+  dt: number;
+  sys: {
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  name: string;
+};
+
+const getWeather = (): Promise<AxiosResponse<WeatherProps>> => {
   return axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=recife
     &appid=${import.meta.env.VITE_API_KEY}&units=metric&lang=pt_br`,
   );
 };
 
-const getWeatherByCity = (city: string) => {
+const getWeatherByCity = (
+  city: string,
+): Promise<AxiosResponse<WeatherProps>> => {
   return axios.get(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}
     &appid=${import.meta.env.VITE_API_KEY}&units=metric&lang=pt_br`,
