157963a591e3482b7b7a846aae60d8e9a6eaa615
azuredeviil053 <azuredevil053@gmail.com>
2023-09-16 17:57:46 -0300

    feat: SearchBar box-shadow

--- src/components/SearchBar/styles.ts
+++ src/components/SearchBar/styles.ts
@@ -4,6 +4,9 @@ import { motion } from 'framer-motion';
 export const Container = styled(motion.form)`
   align-items: center;
   background-color: ${({ theme }) => theme.colors.secundary};
+  box-shadow:
+    0 4px 6px -1px rgb(0 0 0 / 5%),
+    0 2px 4px -2px rgb(0 0 0 / 5%);
   display: flex;
   flex-flow: row;
   justify-content: space-between;
